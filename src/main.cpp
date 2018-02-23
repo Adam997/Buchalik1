0e3d36b8da9e9bdd223dcaf2e110338d25bbcd8f
student <student@mini-iris-06.local>
2018-02-23 14:34:23 +0100

    commit3

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
+
+#include "LaborkaConfig.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+#else
+#include "cmath"
+#define _USE_MATH_DEFINES
+#endif
 int main(){
+    
+    
+#ifdef USE_TRIGONOMETRY_DEGREE
     double a=degreemath::sin( 0);
     double b=degreemath::cos( 45);
     double c=degreemath::tan( 90);
-    double d=degreemath::ctan( 90);
+    
+#else
+    double a=std::sin( 0);
+    double b=std::cos( 45);
+    double c=std::tan( 90);
+#endif
     
     return a;
     
